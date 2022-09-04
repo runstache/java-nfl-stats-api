855e09d8d0d32e36ed639debfa29e9671eeddfb1
Larry Shields <larry.shields@gmail.com>
2022-09-04 12:59:22 -0400

    Project Setup.

---
+++ src/main/java/com/lswebworld/nflstatsapi/NflStatsApiApplication.java
@@ -0,0 +1,18 @@
+package com.lswebworld.nflstatsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+/**
+ * Spring Application.
+ */
+@SpringBootApplication
+@EntityScan("com.lswebworld")
+public class NflStatsApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NflStatsApiApplication.class, args);
+  }
+
+}
