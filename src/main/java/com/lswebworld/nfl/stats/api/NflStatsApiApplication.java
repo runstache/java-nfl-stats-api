0d592255e10fc6061b53a2bd2d69c7377325de4e
Larry Shields <larry.shields@gmail.com>
2022-09-05 08:16:03 -0400

    Project Setup.

--- src/main/java/com/lswebworld/nfl/stats/api/NflStatsApiApplication.java
+++ src/main/java/com/lswebworld/nfl/stats/api/NflStatsApiApplication.java
@@ -1,4 +1,4 @@
-package com.lswebworld.nflstatsapi;
+package com.lswebworld.nfl.stats.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
